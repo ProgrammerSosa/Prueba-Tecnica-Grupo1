b701bb23e901f8fdc5a0ed232411c6c801dbd601
devfwilson <wflorian-2024392@kinal.edu.gt>
2026-07-15 15:05:15 -0600

    feat: agregar crud de productos

--- service-inventory/configs/app.js
+++ service-inventory/configs/app.js
@@ -7,7 +7,7 @@ import { corsOptions } from './cors-configuration.js';
 import { helmetConfiguration } from './helmet-configuration.js';
 import { requestLimit } from '../middlewares/request-limit.js';
 import { errorHandler } from '../middlewares/handle-errors.js';
-import userRoutes from '../src/users/user.routes.js';
+import productRoutes from '../src/products/product.routes.js';
 
 const BASE_PATH = '/api/v1';
 
@@ -20,7 +20,7 @@ const middlewares = (app) => {
 }
 
 const routes = (app) => {
-    app.use(`${BASE_PATH}/users`, userRoutes);
+    app.use(`${BASE_PATH}/products`, productRoutes);
 
     app.get(`${BASE_PATH}/health`, (request, response) => {
         response.status(200).json({
